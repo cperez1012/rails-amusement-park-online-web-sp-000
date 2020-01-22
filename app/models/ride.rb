f7ad64880aaf11e8f086ef79fc18bf74fa37ab35
Christian Perez <perezchristian1012@gmail.com>
2020-01-22 17:34:29 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -11,7 +11,7 @@ class Ride < ActiveRecord::Base
       "Sorry. You do not have enough tickets to ride the #{attraction.name}."
     elsif user.height < attraction.min_height
       "Sorry. You are not tall enough to ride the #{attraction.name}."
-    else 
+    else
       user.tickets -= attraction.tickets
       user.nausea += attraction.nausea_rating
       user.happiness += attraction.happiness_rating
