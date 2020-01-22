19bd71aa6d0c598ec24fd1feb953169bc5ee392c
Christian Perez <perezchristian1012@gmail.com>
2020-01-22 18:15:03 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'application#home' as: 'root'
+  get '/' => 'application#home', as: 'root'
 
   get '/signin' => 'sessions#new', as: 'sign_in'
 
